63e0c3fa8fbe4ac3a26270375745bdb990a8c733
Lawrence Lim <ll4715@nyu.edu>
2022-08-17 17:41:50 +0800

    Launched and deployed basic NFT

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NFT Hardhat Project
+
+## 3 Types of NFT
+
+1. Basic NFT
+2. Random IPES
+3. Dynamic SVG NFT
---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,28 @@
+const { network } = require('hardhat')
+const { networkConfig, developmentChains } = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+
+  log('---------------------------')
+  const args = []
+  const basicNFT = await deploy('BasicNft', {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log('Verifying...')
+    await verify(basicNFT.address, args)
+  }
+}
+
+module.exports.tags = ['all']
